c0418003ae3e7d9eb6805c3a89a26aa1ef9bdf9b
jerrychuang97 <jerrychuang.ana@gmail.com>
2021-03-02 09:09:55 -0500

    Corrected

--- 01_quantification/run_alevin_timecourse.sh
+++ 01_quantification/run_alevin_timecourse.sh
@@ -17,7 +17,7 @@ salmon alevin -l ISR \
 
     
 #P11
-
+cd /media/2/Mm_Timecourse_male_ileum_LMMP/p11
 salmon alevin -l ISR \
     --index /media/2/Reference/mm/vM25/salmon_index \
     -1 p11_1_S7_L001_R1_001.fastq.gz p11_1_S7_L002_R1_001.fastq.gz p11_2_S22_L001_R1_001.fastq.gz p11_2_S22_L002_R1_001.fastq.gz p11_3_S23_L001_R1_001.fastq.gz p11_3_S23_L002_R1_001.fastq.gz p11_4_S15_L001_R1_001.fastq.gz p11_4_S15_L002_R1_001.fastq.gz p11_a_S23_L001_R1_001.fastq.gz p11_a_S23_L002_R1_001.fastq.gz p11_b_S14_L001_R1_001.fastq.gz p11_b_S14_L002_R1_001.fastq.gz p11_c_S21_L001_R1_001.fastq.gz p11_c_S21_L002_R1_001.fastq.gz p11_d_S13_L001_R1_001.fastq.gz p11_d_S13_L002_R1_001.fastq.gz \
@@ -30,10 +30,11 @@ salmon alevin -l ISR \
     --numCellBootstraps 30
     
 #P20
+cd /media/2/Mm_Timecourse_male_ileum_LMMP/p20
 salmon alevin -l ISR \
     --index /media/2/Reference/mm/vM25/salmon_index \
     -1 p20_1_S6_L001_R1_001.fastq.gz p20_1_S6_L002_R1_001.fastq.gz p20_2_S5_L001_R1_001.fastq.gz p20_2_S5_L002_R1_001.fastq.gz p20_3_S13_L001_R1_001.fastq.gz p20_3_S13_L002_R1_001.fastq.gz p20_4_S21_L001_R1_001.fastq.gz p20_4_S21_L002_R1_001.fastq.gz p20_a_S12_L001_R1_001.fastq.gz p20_a_S12_L002_R1_001.fastq.gz p20_b_S15_L001_R1_001.fastq.gz p20_b_S15_L002_R1_001.fastq.gz p20_c_S17_L001_R1_001.fastq.gz p20_c_S17_L002_R1_001.fastq.gz p20_d_S9_L001_R1_001.fastq.gz p20_d_S9_L002_R1_001.fastq.gz \
-    -2 p20_1_S6_L001_R1_001.fastq.gz p20_1_S6_L002_R1_001.fastq.gz p20_2_S5_L001_R1_001.fastq.gz p20_2_S5_L002_R1_001.fastq.gz p20_3_S13_L001_R1_001.fastq.gz p20_3_S13_L002_R1_001.fastq.gz p20_4_S21_L001_R1_001.fastq.gz p20_4_S21_L002_R1_001.fastq.gz p20_a_S12_L001_R1_001.fastq.gz p20_a_S12_L002_R1_001.fastq.gz p20_b_S15_L001_R1_001.fastq.gz p20_b_S15_L002_R1_001.fastq.gz p20_c_S17_L001_R1_001.fastq.gz p20_c_S17_L002_R1_001.fastq.gz p20_d_S9_L001_R1_001.fastq.gz p20_d_S9_L002_R1_001.fastq.gz \
+    -2 p20_1_S6_L001_R2_001.fastq.gz p20_1_S6_L002_R2_001.fastq.gz p20_2_S5_L001_R2_001.fastq.gz p20_2_S5_L002_R2_001.fastq.gz p20_3_S13_L001_R2_001.fastq.gz p20_3_S13_L002_R2_001.fastq.gz p20_4_S21_L001_R2_001.fastq.gz p20_4_S21_L002_R2_001.fastq.gz p20_a_S12_L001_R2_001.fastq.gz p20_a_S12_L002_R2_001.fastq.gz p20_b_S15_L001_R2_001.fastq.gz p20_b_S15_L002_R2_001.fastq.gz p20_c_S17_L001_R2_001.fastq.gz p20_c_S17_L002_R2_001.fastq.gz p20_d_S9_L001_R2_001.fastq.gz p20_d_S9_L002_R2_001.fastq.gz \
     --tgMap /media/2/Reference/mm/vM25/txp2gene.tsv \
     --chromiumV3 \
     --threads 6\
@@ -42,6 +43,7 @@ salmon alevin -l ISR \
     --numCellBootstraps 30
     
 #P21
+cd /media/2/Mm_Timecourse_male_ileum_LMMP/p21
 salmon alevin -l ISR \
     --index /media/2/Reference/mm/vM25/salmon_index \
     -1 p21_1_S8_L001_R1_001.fastq.gz p21_1_S8_L002_R1_001.fastq.gz p21_2_S10_L001_R1_001.fastq.gz p21_2_S10_L002_R1_001.fastq.gz p21_3_S12_L001_R1_001.fastq.gz p21_3_S12_L002_R1_001.fastq.gz p21_4_S2_L001_R1_001.fastq.gz p21_4_S2_L002_R1_001.fastq.gz p21_a_S4_L001_R1_001.fastq.gz p21_a_S4_L002_R1_001.fastq.gz p21_b_S5_L001_R1_001.fastq.gz p21_b_S5_L002_R1_001.fastq.gz p21_c_S10_L001_R1_001.fastq.gz p21_c_S10_L002_R1_001.fastq.gz p21_d_S20_L001_R1_001.fastq.gz p21_d_S20_L002_R1_001.fastq.gz \
@@ -54,6 +56,7 @@ salmon alevin -l ISR \
     --numCellBootstraps 30
     
 #P60
+cd /media/2/Mm_Timecourse_male_ileum_LMMP/p60
 salmon alevin -l ISR \
     --index /media/2/Reference/mm/vM25/salmon_index \
     -1 p60_1_S24_L001_R1_001.fastq.gz p60_1_S24_L002_R1_001.fastq.gz p60_2_S16_L001_R1_001.fastq.gz p60_2_S16_L002_R1_001.fastq.gz p60_3_S18_L001_R1_001.fastq.gz p60_3_S18_L002_R1_001.fastq.gz p60_4_S14_L001_R1_001.fastq.gz p60_4_S14_L002_R1_001.fastq.gz p60_a_S8_L001_R1_001.fastq.gz p60_a_S8_L002_R1_001.fastq.gz p60_b_S18_L001_R1_001.fastq.gz p60_b_S18_L002_R1_001.fastq.gz p60_c_S19_L001_R1_001.fastq.gz p60_c_S19_L002_R1_001.fastq.gz p60_d_S1_L001_R1_001.fastq.gz p60_d_S1_L002_R1_001.fastq.gz \
@@ -66,6 +69,7 @@ salmon alevin -l ISR \
     --numCellBootstraps 30
 
 #P61
+cd /media/2/Mm_Timecourse_male_ileum_LMMP/p61
 salmon alevin -l ISR \
     --index /media/2/Reference/mm/vM25/salmon_index \
     -1 p61_1_S19_L001_R1_001.fastq.gz p61_1_S19_L002_R1_001.fastq.gz p61_2_S17_L001_R1_001.fastq.gz p61_2_S17_L002_R1_001.fastq.gz p61_3_S1_L001_R1_001.fastq.gz p61_3_S1_L002_R1_001.fastq.gz p61_4_S4_L001_R1_001.fastq.gz p61_4_S4_L002_R1_001.fastq.gz p61_a_S6_L001_R1_001.fastq.gz p61_a_S6_L002_R1_001.fastq.gz p61_b_S16_L001_R1_001.fastq.gz p61_b_S16_L002_R1_001.fastq.gz p61_c_S11_L001_R1_001.fastq.gz p61_c_S11_L002_R1_001.fastq.gz p61_d_S2_L001_R1_001.fastq.gz p61_d_S2_L002_R1_001.fastq.gz \
